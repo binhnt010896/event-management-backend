bc653cecda79234e05f759658ca4ed152ccc0f63
Binh Nguyen <binhnt.010896@gmail.com>
2025-01-13 22:21:09 +0700

    sync

--- models/Notification.js
+++ models/Notification.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/db.config');
 
 const Notification = sequelize.define('Notification', {
   userId: {
